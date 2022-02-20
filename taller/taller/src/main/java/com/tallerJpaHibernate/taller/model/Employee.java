27e9117a7059bf6e5dee377d1b0a58be6eb6b6c3
jf98uni <jf.botero@uniandes.edu.co>
2022-02-20 11:09:57 -0500

    Creacion del model y la clase Employee

---
+++ taller/taller/src/main/java/com/tallerJpaHibernate/taller/model/Employee.java
@@ -0,0 +1,85 @@
+package com.tallerJpaHibernate.taller.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 25,nullable = false)
+    private String firstName;
+
+    @Column(length = 25,nullable = false)
+    private String lastName;
+
+    @Column(length =10,nullable = false,unique = true)
+    private String employeeid;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String employeeid) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeid = employeeid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(String employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return getId().equals(employee.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", employeeid='" + employeeid + '\'' +
+                '}';
+    }
+}
